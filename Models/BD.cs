ae97ad962390f8ea84e6b34c0f41b9f75a69e763
MAT�AS JOAQUIN CASTERA <48385887@ort>
2024-09-05 16:01:45 -0300

    .

--- Models/BD.cs
+++ Models/BD.cs
@@ -3,4 +3,12 @@ using Dapper;
 namespace TP07_PreguntadORT;
 static class BD{
         private static string _connectionString = @"Server=localhost; DataBase=BD-PreguntadORT; Trusted_Connection=True;";
+public static List<Categoria> ObtenerCategorias(){
+        List<Categoria> ObtenerCategorias = new List<Categoria>();
+        string SQL = "SELECT Nombre FROM Categorias";
+        using(SqlConnection db =new SqlConnection(_connectionString)){
+                db.Execute(SQL,new{pIdDeportista = idDeportista} );
+        }
+}
+
 }
